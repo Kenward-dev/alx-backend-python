ae05fce97135e1fb2edfe31b9e9797b626184306
Kenward Terhemba <codewithkenward@gmail.com>
2025-05-14 10:35:45 +0100

    add batch processing script for large data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    """Generator that streams rows from the 'user_data' table in batches."""
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="ALX_prodev",
+        consume_results=True
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    """Function to process user data in batches."""
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            print(user)
+
+
