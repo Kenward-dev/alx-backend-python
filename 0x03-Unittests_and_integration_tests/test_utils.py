ee8abc7b3c28c94bc1362ade29ee02c1caf92090
Kenward Terhemba <codewithkenward@gmail.com>
2025-06-02 13:59:23 +0100

    add project structure

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Unit tests for utils module."""
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
