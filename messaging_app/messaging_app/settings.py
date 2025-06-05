48dd261b0b4daad97932ef05b87d26979b104b7e
Kenward Terhemba <codewithkenward@gmail.com>
2025-06-05 16:05:09 +0100

    modify global pagination settings to DRF default pagination

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagesPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20, 
 }
 
